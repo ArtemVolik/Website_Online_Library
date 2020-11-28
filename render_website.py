a7636945247e6d9f48f9392220ec573fb50053fc
artyom <art.volyk@gmail.com>
2020-11-28 21:45:04 +0200

    render index.html to pages

--- render_website.py
+++ render_website.py
@@ -28,7 +28,7 @@ def make_page_content(quantity_on_page: int, books: list, template, directory):
 
         # for git pages
         if page == 1:
-            page_path = 'index.html'
+            page_path = os.path.join(directory, 'index.html')
             with open(page_path, 'w', encoding="utf8") as file:
                 file.write(rendered_page)
 
